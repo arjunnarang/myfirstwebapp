79c6736d120c23483b14593089780f6b0bc0b0bd
arjunnarang <arjun.12narang@gmail.com>
2024-08-24 23:40:17 +0530

    Add Validations to bean and display validation errors in view(todo.jsp

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -50,7 +53,13 @@ public class TodoController {
 	//those inputs will handled by this function
 	@RequestMapping(value="/add-todo", method=RequestMethod.POST)
 //	public String addNewTodo(@RequestParam String description, ModelMap model) {
-    public String addNewTodo(ModelMap model, Todo todo) {
+	// @Valid is for validation
+    public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		
+		//BindingResult helps in handling Size errors
+		if(result.hasErrors()) {
+			return "todo";
+		}
 		//Getting username from model
 		String username = (String) model.get("name");
 		
