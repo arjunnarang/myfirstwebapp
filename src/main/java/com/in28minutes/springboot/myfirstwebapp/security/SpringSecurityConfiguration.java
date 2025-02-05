8b0ae1f9b49f07c861f1464038a00f3b6c08815c
arjunnarang <arjun.12narang@gmail.com>
2025-02-05 23:53:31 +0530

    Added new user

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -23,8 +23,9 @@ public class SpringSecurityConfiguration {
 		
 		UserDetails userDetails1 = createNewUser("in28minutes","dummy");
 		UserDetails userDetails2 = createNewUser("Arjun","dummy");
+		UserDetails userDetails3 = createNewUser("user","password");
 		
-		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2, userDetails3);
 	}
 
 	
